f83686f1e38ee12f80d05ee8f4b5351d0d26e956
449191084@qq.com <449191084@qq.com>
2017-02-09 09:48:06 +0800

    文件修改

---
+++ DriveriOSApp/UI/Indent/LXQReservationIndentTips.h
@@ -0,0 +1,13 @@
+//
+//  LXQReservationIndentTips.h
+//  DriveriOSApp
+//
+//  Created by mac on 17/2/8.
+//  Copyright © 2017年 陆遗坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXQReservationIndentTips : UIView
+
+@end
