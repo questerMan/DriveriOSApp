e91a44a8b73fba2312088ed033b394464ce108a2
questerman <luyikun01@163.com>
2017-02-16 13:10:12 +0800

    login

--- DriveriOSApp/UI/Other/Login/Login.m
+++ DriveriOSApp/UI/Other/Login/Login.m
@@ -32,7 +32,7 @@
 
 -(UIImageView *)logoIMG{
     if (!_logoIMG) {
-        _logoIMG = [[UIImageView alloc] initWithFrame:CGRectMake((SCREEN_W - MATCHSIZE(120))/2, MATCHSIZE(225), MATCHSIZE(120), MATCHSIZE(120))];
+        _logoIMG = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo"]];
     }
     return _logoIMG;
 }
@@ -64,9 +64,11 @@
 
 -(UIButton *)sendCodeBtn{
     if (!_sendCodeBtn) {
-        _sendCodeBtn = [FactoryClass buttonWithFrame:CGRectMake(SCREEN_W - MATCHSIZE(340) - MATCHSIZE(120) , MATCHSIZE(254), MATCHSIZE(180), MATCHSIZE(70)) Title:@"获取验证码" backGround:[UIColor grayColor] tintColor:[UIColor blackColor] cornerRadius:MATCHSIZE(2)];
-        [_sendCodeBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-        _sendCodeBtn.titleLabel.font = [UIFont systemFontOfSize:MATCHSIZE(25)];
+        _sendCodeBtn = [FactoryClass buttonWithFrame:CGRectMake(SCREEN_W - MATCHSIZE(340) - MATCHSIZE(120) , MATCHSIZE(254), MATCHSIZE(180), MATCHSIZE(70)) Title:@"获取验证码" backGround:[UIColor clearColor] tintColor:[UIColor blackColor] cornerRadius:MATCHSIZE(0)];
+        [_sendCodeBtn setTitleColor:UIColorFromRGB(@"#ff772b") forState:UIControlStateNormal];
+        _sendCodeBtn.titleLabel.font = [UIFont systemFontOfSize:MATCHSIZE(26)];
+        _sendCodeBtn.layer.borderWidth = MATCHSIZE(1.5);
+        _sendCodeBtn.layer.borderColor = UIColorFromRGB(@"#e0e0e0").CGColor;
     }
     return _sendCodeBtn;
 }
@@ -74,16 +76,17 @@
 
 -(UIButton *)loginBtn{
     if (!_loginBtn) {
-        _loginBtn = [FactoryClass buttonWithFrame:CGRectMake((SCREEN_W - MATCHSIZE(220) - MATCHSIZE(100))/2, MATCHSIZE(384), MATCHSIZE(100), MATCHSIZE(40)) Title:@"登录" backGround:[UIColor clearColor] tintColor:[UIColor blackColor] cornerRadius:MATCHSIZE(8)];
-        [_loginBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-        _loginBtn.titleLabel.font = [UIFont systemFontOfSize:MATCHSIZE(30)];
+        _loginBtn = [FactoryClass buttonWithFrame:CGRectMake((SCREEN_W - MATCHSIZE(220) - MATCHSIZE(100))/2, MATCHSIZE(384), MATCHSIZE(100), MATCHSIZE(40)) Title:@"登录" backGround:[UIColor clearColor] tintColor:[UIColor blackColor] cornerRadius:MATCHSIZE(0)];
+        [_loginBtn setTitleColor:UIColorFromRGB(@"#ff772b") forState:UIControlStateNormal];
+        _loginBtn.titleLabel.font = [UIFont systemFontOfSize:MATCHSIZE(26)];
     }
     return _loginBtn;
 }
 
 -(UIButton *)eyeBtn{
     if (!_eyeBtn) {
-        _eyeBtn = [FactoryClass buttonWithFrame:CGRectMake(0, MATCHSIZE(20), MATCHSIZE(40), MATCHSIZE(40)) image:[UIImage imageNamed:@"eye"]];
+        _eyeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_eyeBtn setImage:[UIImage imageNamed:@"eyes"] forState:UIControlStateNormal];
     }
     return _eyeBtn;
 }
@@ -117,58 +120,150 @@
     self.view.backgroundColor = [UIColor grayColor];
     //背景图
     UIImageView *bgIMG = [[UIImageView alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    bgIMG.image = [UIImage imageNamed:@"loginBG"];
+    bgIMG.image = [UIImage imageNamed:@"baseboard"];
     [self.view addSubview:bgIMG];
     
     //logo图
     [self.view addSubview:self.logoIMG];
-    self.logoIMG.image = [UIImage imageNamed:@"logo"];
+    [self.logoIMG mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view).offset(MATCHSIZE(308));
+        make.top.equalTo(self.view).offset(MATCHSIZE(124));
+        make.right.equalTo(self.view).offset(MATCHSIZE(-308));
+        make.height.offset(MATCHSIZE(180));
+    }];
+
     
-    UIView *bgView = [[UIView alloc] initWithFrame:CGRectMake(MATCHSIZE(100), MATCHSIZE(600), SCREEN_W - MATCHSIZE(200), MATCHSIZE(450))];
+    UIView *bgView = [[UIView alloc] init];
     bgView.backgroundColor = [UIColor whiteColor];
     bgView.layer.cornerRadius = MATCHSIZE(10);
     bgView.layer.masksToBounds = YES;
     [self.view addSubview:bgView];
+    [bgView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.logoIMG.mas_bottom).offset(MATCHSIZE(105));
+        make.left.equalTo(self.view).offset(MATCHSIZE(74));
+        make.right.equalTo(self.view).offset(MATCHSIZE(-74));
+        make.height.offset(MATCHSIZE(466));
+    }];
     
-    UILabel *userNameLab = [FactoryClass labelWithText:@"手机号" fontSize:MATCHSIZE(30) textColor:[UIColor blackColor] numberOfLine:1 textAlignment:NSTextAlignmentLeft backGroundColor:[UIColor clearColor]];
-    userNameLab.frame = CGRectMake(MATCHSIZE(60), MATCHSIZE(30), MATCHSIZE(200), MATCHSIZE(40));
+    UIImageView *userImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"phone"]];
+    [bgView addSubview:userImg];
+    [userImg mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(bgView).offset(MATCHSIZE(36));
+        make.left.equalTo(bgView).offset(MATCHSIZE(40));
+        make.width.offset(MATCHSIZE(30));
+        make.height.offset(MATCHSIZE(30));
+    }];
+    
+    UILabel *userNameLab = [FactoryClass labelWithText:@"手机号" fontSize:MATCHSIZE(32) textColor:UIColorFromRGB(@"#ff772b") numberOfLine:1 textAlignment:NSTextAlignmentLeft backGroundColor:[UIColor clearColor]];
     [bgView addSubview:userNameLab];
+    [userNameLab mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(bgView).offset(MATCHSIZE(36));
+        make.left.equalTo(userImg.mas_right).offset(MATCHSIZE(20));
+        make.width.offset(MATCHSIZE(100));
+        make.height.offset(MATCHSIZE(30));
+    }];
+    
     //手机文本框
     [bgView addSubview:self.usersF];
+    [self.usersF mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(userImg.mas_bottom).offset(MATCHSIZE(0));
+        make.left.equalTo(bgView).offset(MATCHSIZE(80));
+        make.right.equalTo(bgView).offset(MATCHSIZE(-80));
+        make.height.offset(MATCHSIZE(70));
+    }];
     
-    UILabel *line = [FactoryClass labelWithText:@"" fontSize:MATCHSIZE(30) textColor:[UIColor blackColor] numberOfLine:1 textAlignment:NSTextAlignmentLeft backGroundColor:[UIColor blackColor]];
-    line.frame = CGRectMake(MATCHSIZE(60), MATCHSIZE(121), VIEW_W - MATCHSIZE(340), MATCHSIZE(2));
+    UILabel *line = [FactoryClass labelWithText:@"" fontSize:MATCHSIZE(30) textColor:[UIColor blackColor] numberOfLine:1 textAlignment:NSTextAlignmentLeft backGroundColor:UIColorFromRGB(@"#e0e0e0")];
     [bgView addSubview:line];
+    [line mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(userImg.mas_bottom).offset(MATCHSIZE(70));
+        make.left.equalTo(bgView).offset(MATCHSIZE(40));
+        make.right.equalTo(bgView).offset(MATCHSIZE(-40));
+        make.height.offset(MATCHSIZE(1.5));
+    }];
+   
+    //锁头图片
+    UIImageView *pwdImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"password"]];
+    [bgView addSubview:pwdImg];
+    [pwdImg mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(line.mas_bottom).offset(MATCHSIZE(30));
+        make.left.equalTo(bgView).offset(MATCHSIZE(40));
+        make.width.offset(MATCHSIZE(30));
+        make.height.offset(MATCHSIZE(30));
+    }];
     
-    UILabel *userPassLab = [FactoryClass labelWithText:@"密  码" fontSize:MATCHSIZE(30) textColor:[UIColor blackColor] numberOfLine:1 textAlignment:NSTextAlignmentLeft backGroundColor:[UIColor clearColor]];
-    userPassLab.frame = CGRectMake(MATCHSIZE(60), MATCHSIZE(133), MATCHSIZE(200), MATCHSIZE(40));
+    UILabel *userPassLab = [FactoryClass labelWithText:@"密码" fontSize:MATCHSIZE(30) textColor:UIColorFromRGB(@"#ff772b") numberOfLine:1 textAlignment:NSTextAlignmentLeft backGroundColor:[UIColor clearColor]];
     [bgView addSubview:userPassLab];
+    [userPassLab mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(line.mas_bottom).offset(MATCHSIZE(30));
+        make.left.equalTo(pwdImg.mas_right).offset(MATCHSIZE(20));
+        make.width.offset(MATCHSIZE(100));
+        make.height.offset(MATCHSIZE(30));
+    }];
+    
     //密码文本框
     [bgView addSubview:self.passWordF];
+    [self.passWordF mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(pwdImg.mas_bottom).offset(MATCHSIZE(0));
+        make.left.equalTo(bgView).offset(MATCHSIZE(80));
+        make.right.equalTo(bgView).offset(MATCHSIZE(-80));
+        make.height.offset(MATCHSIZE(70));
+    }];
     
-    UILabel *line2 = [FactoryClass labelWithText:@"" fontSize:MATCHSIZE(30) textColor:[UIColor blackColor] numberOfLine:1 textAlignment:NSTextAlignmentLeft backGroundColor:[UIColor blackColor]];
-    line2.frame = CGRectMake(MATCHSIZE(60), MATCHSIZE(224), VIEW_W - MATCHSIZE(340), MATCHSIZE(2));
+    UILabel *line2 = [FactoryClass labelWithText:@"" fontSize:MATCHSIZE(30) textColor:[UIColor blackColor] numberOfLine:1 textAlignment:NSTextAlignmentLeft backGroundColor:UIColorFromRGB(@"#e0e0e0")];
     [bgView addSubview:line2];
+    [line2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(pwdImg.mas_bottom).offset(MATCHSIZE(70));
+        make.left.equalTo(bgView).offset(MATCHSIZE(40));
+        make.right.equalTo(bgView).offset(MATCHSIZE(-40));
+        make.height.offset(MATCHSIZE(1.5));
+    }];
+    
+   
     //验证码文本框
     [bgView addSubview:self.codeF];
+    [self.codeF mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(line2.mas_bottom).offset(MATCHSIZE(20));
+        make.left.equalTo(bgView).offset(MATCHSIZE(40));
+        make.width.offset(MATCHSIZE(170));
+        make.height.offset(MATCHSIZE(56));
+    }];
     
-    UILabel *line3 = [FactoryClass labelWithText:@"" fontSize:MATCHSIZE(30) textColor:[UIColor blackColor] numberOfLine:1 textAlignment:NSTextAlignmentLeft backGroundColor:[UIColor blackColor]];
-    line3.frame = CGRectMake(MATCHSIZE(60), MATCHSIZE(325), MATCHSIZE(160), MATCHSIZE(2));
+    UILabel *line3 = [FactoryClass labelWithText:@"" fontSize:MATCHSIZE(30) textColor:[UIColor blackColor] numberOfLine:1 textAlignment:NSTextAlignmentLeft backGroundColor:UIColorFromRGB(@"#e0e0e0")];
     [bgView addSubview:line3];
+    [line3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(line2.mas_bottom).offset(MATCHSIZE(76));
+        make.left.equalTo(bgView).offset(MATCHSIZE(40));
+        make.width.offset(MATCHSIZE(170));
+        make.height.offset(MATCHSIZE(1.5));
+    }];
     
     //发送验证码
     [bgView addSubview:self.sendCodeBtn];
+    [self.sendCodeBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(line2.mas_bottom).offset(MATCHSIZE(20));
+        make.left.equalTo(line3.mas_right).offset(MATCHSIZE(58));
+        make.width.offset(MATCHSIZE(154));
+        make.height.offset(MATCHSIZE(58));
+    }];
+    
     //发送验证码按钮点击事件
     [[self.sendCodeBtn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
         
         UIButton *btn = (UIButton *)x;
-        [btn countDownFromTime:60 title:@"获取验证码" unitTitle:@"s后重新获取" mainColor:[UIColor grayColor] countColor:[UIColor grayColor]];
+        [btn countDownFromTime:60 title:@"获取验证码" unitTitle:@"s后重新获取" mainColor:[UIColor clearColor] countColor:[UIColor clearColor]];
         
     }];
     
     
     //登录
     [bgView addSubview:self.loginBtn];
+    [self.loginBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(bgView.mas_bottom).offset(MATCHSIZE(-36));
+        make.height.offset(MATCHSIZE(40));
+        make.width.offset(MATCHSIZE(200));
+        make.centerX.offset(0);
+    }];
+    
     [[self.loginBtn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
         //跳到主页面，移除当前登录页面
         [self.view removeFromSuperview];
@@ -183,6 +278,13 @@
         
     }];
     
+    [bgView addSubview:self.eyeBtn];
+    [self.eyeBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(line2.mas_top).offset(MATCHSIZE(-30));
+        make.right.equalTo(bgView).offset(MATCHSIZE(-40));
+        make.width.offset(MATCHSIZE(20));
+        make.height.offset(MATCHSIZE(10));
+    }];
     
     
 }
